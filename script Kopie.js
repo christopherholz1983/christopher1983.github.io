5f21753e2637e7c39ee2c63e0c8ee27942382f3f
christopherholz1983 <chrissi_holz@web.de>
2025-01-06 13:28:00 +0100

    reupload

--- script Kopie.js
+++
@@ -1,8 +0,0 @@
-const hamburger = document.getElementById("hamburger");
-const mobileMenu = document.getElementById("mobileMenu");
-
-hamburger.addEventListener("click",()=>{
-mobileMenu.classList.toggle("active");
-hamburger.classList.toggle("active");
-
-});
